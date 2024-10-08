a081d8ec216123c24c421030c917ee86b8784ca0
Cyntaelis <67953493+Cyntaelis@users.noreply.github.com>
2024-10-08 15:30:15 -0400

    PM10 now handled as optional
    More PM2.5 only sensors were added, so PM10 data is no longer guaranteed.

--- src/aqi.py
+++ src/aqi.py
@@ -20,7 +20,8 @@ def get_aqi(zip_code):
         return current
 
     def fmt_current(gc):
-        return f'{zip_code}\nOzone: {gc["Ozone"][:2]}\nPM2.5: {gc["PM2.5"][:2]}\nPM10:  {gc["PM10"][:2]}'
+        return f'{zip_code}\nOzone: {gc["Ozone"][:2]}\nPM2.5: {gc["PM2.5"][:2]}\n{
+            "PM10:  "+str(gc["PM10"][:2]) if "PM10" in gc else ""}'
         
     try:
         gc = get_current(zip_code)
